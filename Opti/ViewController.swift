7d5a22013aedd2f926c66cdcd7983e8a7a2d97df
Emils Beloglazovs <18eab7@queensu.ca>
2020-01-19 21:38:16 -0500

    Minimalistic Gym App

---
+++ Opti/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Opti
+//
+//  Created by Emils Beloglazovs on 2020-01-11.
+//  Copyright © 2020 Emils Beloglazovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
